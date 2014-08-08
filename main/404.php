ec00fe4bd0de6786680678785cb6f539b1ed2395
SeryogaGit <seryoga_ne_@mailru>
2014-08-08 18:02:18 +0300

    404

---
+++ main/404.php
@@ -0,0 +1,12 @@
+﻿<?php include("_header.html"); ?>
+<div class="center page404">
+	<img src="../img/404.png" alt="">
+	<div class="error404">Ошибка 404. Страница не найдена</div>
+	<div class="desc-404">Такой страницы не существует.<br> Она могла быть перемещена, удалена или как-то еще модифицирована.</div>
+	<div class="link-404">
+		<a href="#">Перейдите на главную страницу</a>
+		или
+		<a href="#">воспользуйтесь картой сайта</a>
+	</div>
+</div> <!-- END CENTER -->
+<?php include("_footer.html"); ?>
