72695a85f54769ca91a4b139e89da44c16356e8e
SeryogaGit <seryoga_ne_@mailru>
2014-08-11 19:15:45 +0300

    sitemap

---
+++ main/sitemap.php
@@ -0,0 +1,49 @@
+﻿<?php include("_header.html"); ?>
+<div class="center sitemap-center">
+	<div class="sitemap-title">Карта сайта</div>
+	<div class="main-page">
+		<a href="#">Главная страница</a>
+	</div>
+	<img src="../img/sitemap.png" alt="" class="sitemap">
+	<div class="block-map">
+		<div class="title-map">
+			<a href="#">Продукты</a>
+		</div>
+		<a href="#">Категория 1</a><br>
+		<a href="#">Категория N</a>
+	</div>
+	<div class="block-map">
+		<div class="title-map">
+			<a href="#">Аренда ПО</a>
+		</div>
+	</div>
+	<div class="block-map">
+		<div class="title-map">
+			<a href="#">Услуги</a>
+		</div>
+		<a href="#">Услугa 1</a><br>
+		<a href="#">Услугa N</a>
+	</div>
+	<div class="block-map">
+		<div class="title-map">
+			<a href="#">Проекты</a>
+		</div>
+		<a href="#">Проект 1</a><br>
+		<a href="#">Проект N</a>
+	</div>
+	<div class="block-map">
+		<div class="title-map">
+			<a href="#">О компании</a>
+		</div>
+		<a href="#">Новости и акции</a><br>
+		<a href="#">Сертификаты</a><br>
+		<a href="#">Партнеры</a><br>
+		<a href="#">Клиенты</a>
+	</div>
+	<div class="block-map">
+		<div class="title-map">
+			<a href="#">Контакты</a>
+		</div>
+	</div>
+</div> <!-- END CENTER -->
+<?php include("_footer.html"); ?>
