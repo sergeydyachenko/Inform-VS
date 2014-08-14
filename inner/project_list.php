f635a5770fdd2334c65b8f4b3a0666d6c8276a1d
SeryogaGit <seryoga_ne_@mailru>
2014-08-14 10:17:13 +0300

    project_list

---
+++ inner/project_list.php
@@ -0,0 +1,91 @@
+﻿<?php include("_header.html"); ?>
+<div class="product-img project-detail-img">
+	<div class="center clearfix">
+		<div class="top-menu content-top-menu">
+			<ul class="product-menu">
+				<li><a href="#">Главная</a></li>   
+				<li>Проекты</li>
+			</ul>
+			<h1>Реализованные проекты</h1>
+		</div>
+	</div>
+</div>
+<div class="center">
+	<div class="wrap-blocks-project-list clearfix">
+		<div class="block-project-list">
+			<div class="project-list">
+				<a class="title-project-list">Название проекта</a>
+				<div class="customer-project">
+					<div>Заказчик:</div>
+					<span>Название заказчика</span>
+					<p>Краткое описание задачи и произведенных работ. Краткое описание задачи и произведенных работ описание задачи и произведенных работ. Краткое описание задачи и произведенных работ. Краткое описание задачи и произведенных работ. Краткое описание задачи и произведенных работ описание задачи и произведенных работ. Краткое описание задачи и произведенных работ. </p>
+				</div>
+			</div>
+			<div class="used-project">
+				<div>Используемые продукты:</div>
+				<div class="link-used-products">
+					<a href="#">Название продукта 1 </a>
+					<div>|</div>
+					<a href="#">Название продукта 1 </a>
+				</div>
+			</div>
+		</div>
+		<div class="block-project-list">
+			<div class="project-list">
+				<a class="title-project-list">Название проекта</a>
+				<div class="customer-project">
+					<div>Заказчик:</div>
+					<span>Название заказчика</span>
+					<p>Краткое описание задачи и произведенных работ. Краткое описание задачи и произведенных работ описание задачи и произведенных работ. Краткое описание задачи и произведенных работ.</p>
+				</div>
+			</div>
+			<div class="used-project">
+				<div>Используемые продукты:</div>
+				<div class="link-used-products">
+					<a href="#">Название продукта 1 </a>
+					<div>|</div>
+					<a href="#">Название продукта 1 </a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="wrap-blocks-project-list clearfix">
+		<div class="block-project-list">
+			<div class="project-list">
+				<a class="title-project-list">Название проекта</a>
+				<div class="customer-project">
+					<div>Заказчик:</div>
+					<span>Название заказчика</span>
+					<p>Краткое описание задачи и произведенных работ. Краткое описание задачи и произведенных работ описание задачи и произведенных работ. Краткое описание задачи и произведенных работ. </p>
+				</div>
+			</div>
+			<div class="used-project">
+				<div>Используемые продукты:</div>
+				<div class="link-used-products">
+					<a href="#">Название продукта 1 </a>
+					<div>|</div>
+					<a href="#">Название продукта 1 </a>
+				</div>
+			</div>
+		</div>
+		<div class="block-project-list">
+			<div class="project-list">
+				<a class="title-project-list">Название проекта</a>
+				<div class="customer-project">
+					<div>Заказчик:</div>
+					<span>Название заказчика</span>
+					<p>Краткое описание задачи и произведенных работ. Краткое описание задачи и произведенных работ описание задачи и произведенных работ. Краткое описание задачи и произведенных работ. </p>
+				</div>
+			</div>
+			<div class="used-project">
+				<div>Используемые продукты:</div>
+				<div class="link-used-products">
+					<a href="#">Название продукта 1 </a>
+					<div>|</div>
+					<a href="#">Название продукта 1 </a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php include("_footer.html"); ?>
