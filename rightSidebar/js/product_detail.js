795c66672c978752d68ffb39b9d959a5c2b5e0c2
SeryogaGit <seryoga_ne_@mailru>
2014-08-14 18:38:14 +0300

    edit product_detail

--- rightSidebar/js/product_detail.js
+++ rightSidebar/js/product_detail.js
@@ -26,9 +26,46 @@ $(document).ready(function(){
 	$(".order").on("click",function(){
 		$(".popup-detail").show();
 	});
+	/* Select */
 	(function($) {
 		$(function() {
 			$('select').styler();
 		});
 	})(jQuery);
+	/* Validator */
+	$(".popup-detail .button-datail").on("click", function(e){
+		// PHONE
+		var phone = $(".popup-detail .tel-number-detail").val();
+		var regPhone = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+		var resultPhone = phone.search(regPhone);
+		if(resultPhone !== 0){
+			e.preventDefault();
+			$(".popup-detail .tel-number-detail").addClass("error");
+		}
+		else{
+			$(".popup-detail .tel-number-detail").removeClass("error");
+		}
+		// COUNT
+		var count = $(".popup-detail .users-count").val();
+		var regCount =  /^\d+$/;
+		var resultCount = count.search(regCount);
+		if(resultCount !== 0){
+			e.preventDefault();
+			$(".popup-detail .users-count").addClass("error");
+		}
+		else{
+			$(".popup-detail .users-count").removeClass("error");
+		}
+
+		if(resultPhone !== 0 || resultCount !== 0){
+			$(".message-error-detail").show();
+		}
+		else{
+			$(".message-error-detail").hide();
+		}
+		if(resultPhone == 0 && resultCount == 0){
+			$(".popup-detail").hide();
+			$(".aside-popup-send").show();
+		}
+	});
 });
