58f97baa32f12f51bc15e2cb265fd0b7f32d98a2
SeryogaGit <seryoga_ne_@mailru>
2014-08-14 18:36:45 +0300

    arenda

---
+++ inner/arenda.php
@@ -0,0 +1,147 @@
+﻿<?php include("_header.html"); ?>
+<div class="popup-detail">
+    <div>
+        <img src="../img/closed.png" alt="" class="closed">
+        <div class="title-popup-detail">Заявка на аренду ПО</div>
+        <div class="help-block">
+            <form>
+                <select>
+                    <option>Готовые решения: Базовый</option>
+                    <option>Название продукта1</option>
+                    <option>Название продукта2</option>
+                    <option>Название продукта3</option>
+                </select>
+                <input type="text" placeholder="Название компании" required="required" class="name-company">
+                <input type="text" placeholder="ФИО">
+                <input type="tel" placeholder="Телефон">
+                <input type="email" placeholder="E-mail">
+                <input type="text" placeholder="Направление деятельности" required="required" class="field-activity">
+                <input type="text" placeholder="Количество пользователей">
+                <textarea placeholder="Описание задачи"></textarea>
+                <div class="clear"></div>
+                <input type="submit" class="button-red button-datail" value="Отправить заявку">
+                <div class="message-error-detail">
+                    <span>Отмеченные поля заполнены неверно.</span>
+                </div>
+            </form>
+        </div>
+    </div>                                                                  
+</div>
+<div class="aside-popup-send">
+    <div>
+        <img src="../img/closed.png" alt="" class="closed">
+        <img src="../img/skills.png" alt="" class="send-img">
+        <p>Ваше сообщение отправлено.</p>
+        <span>Специалисты компании ответят на ваше сообщение в течение 1 часа. <br> Спасибо за обращение в Информ-ВС.</span>
+    </div>                                                                  
+</div>
+<div class="product-img arenda-img">
+	<div class="center clearfix">
+		<div class="top-menu content-top-menu">
+			<ul class="product-menu">
+				<li><a href="#">Главная</a></li>   
+				<li>Аренда ПО</li>
+			</ul>
+			<h1>Аренда ПО</h1>
+		</div>
+	</div>
+</div>
+<div class="center project-list-bottom">
+	<div class="desc-arenda clearfix">
+		<p>Наша компания также занимается разработкой собственных программных продуктов на платформе «1С:Предприятие 8». В нашей компании работают сертифицированные фирмой «1С» специалисты, которые постоянно совершенствуют свои знания и навыки. Они помогут качественно и оперативно решить задачи по автоматизации управления и учета на вашем предприятии. Наша компания опирается в своей работе на знание и повседневное применение стандартов качества, проектных методов в управлении, процессного подхода в организации нашей деятельности.</p>
+		<p>В нашей компании работают сертифицированные фирмой «1С» специалисты, которые постоянно совершенствуют свои знания и навыки. Они помогут качественно и оперативно решить задачи по автоматизации управления и учета на вашем предприятии. Наша компания опирается в своей работе на знание и повседневное применение стандартов качества, проектных методов в управлении, процессного подхода в организации нашей деятельности.</p>
+	</div>
+	<div class="love-block-arenda clearfix">
+		<div></div>
+		<img src="../img/loveC.png" alt="">
+		<div></div>
+	</div>
+	<div class="advantages-cooperation clearfix">
+		<div class="advantages-title">Преимущества сотрудничества с Информ-ВС</div>
+		<div class="advantages-block">
+			<img src="../img/advantage1.png" alt="">
+			<div>Техническая поддержка</div>
+			<p>Наша компания также занимается разработкой собственных программных продуктов на платформе «1С:Предприятие 8».</p>
+		</div>
+		<div class="advantages-block">
+			<img src="../img/advantage2.png" alt="">
+			<div>Защита и конфиденциальность</div>
+			<p>В нашей компании работают сертифицированные фирмой «1С» специалисты, которые постоянно совершенствуют свои знания и навыки. Они помогут качественно и оперативно решить задачи по автоматизации управления и учета на вашем предприятии. </p>
+		</div>
+		<div class="advantages-block">
+			<img src="../img/advantage3.png" alt="">
+			<div>Бесплатное обучение</div>
+			<p>Наша компания опирается в своей работе на знание и повседневное применение стандартов качества, проектных методов в управлении, процессного подхода в организации нашей деятельности.</p>
+		</div>
+	</div>
+	<div class="love-block-arenda clearfix">
+		<div></div>
+		<img src="../img/skills.png" alt="">
+		<div></div>
+	</div>
+	<div class="advantages-title">Готовые серверные решения</div>
+	<div class="server-solutions clearfix">
+		<div class="server-solutions-block">
+			<div class="title-server-solutions">Базовый</div>
+			<p>В нашей компании работают сертифицированные фирмой «1С» специалисты, которые постоянно совершенствуют свои знания и навыки. Они помогут качественно и оперативно решить задачи по автоматизации управления и учета на вашем предприятии. </p>
+			<div class="price-server-solutions">
+				<div>7 000 руб.</div>
+				<span>за одно рабочее место</span>
+			</div>
+			<button class="button-red">Отправить заявку</button>
+		</div>
+		<div class="server-solutions-block">
+			<div class="title-server-solutions">Название решения</div>
+			<p>Наша компания также занимается разработкой собственных программных продуктов на платформе «1С:Предприятие 8».</p>
+			<div class="price-server-solutions">
+				<div>12 000 руб.</div>
+				<span>за одно рабочее место</span>
+			</div>
+			<button class="button-red">Отправить заявку</button>
+		</div>
+		<div class="server-solutions-block">
+			<div class="title-server-solutions">Базовый</div>
+			<p>Наша компания опирается в своей работе на знание и повседневное применение стандартов качества, проектных методов в управлении, процессного подхода в организации нашей деятельности.</p>
+			<div class="price-server-solutions">
+				<div>17 000 руб.</div>
+				<span>за одно рабочее место</span>
+			</div>
+			<button class="button-red">Отправить заявку</button>
+		</div>
+	</div>
+	<div class="clear"></div>
+	<div class="love-block-arenda clearfix persone-love">
+		<div></div>
+		<img src="../img/persons.png" alt="">
+		<div></div>
+	</div> 
+	<div class="individual-solutions clearfix">
+		<div class="advantages-title">индивидуальные решения</div>
+		<p>Наша компания также занимается разработкой собственных программных продуктов на платформе «1С:Предприятие 8». В нашей компании работают сертифицированные фирмой «1С» специалисты, которые постоянно совершенствуют свои знания и навыки. Они помогут качественно и оперативно решить задачи по автоматизации управления и учета на вашем предприятии. Наша компания опирается в своей работе на знание и повседневное применение стандартов качества, проектных методов в управлении, процессного подхода в организации нашей деятельности.</p>
+		<form>
+			<div class="individual-solutions-left">
+				<input type="text" placeholder="Название компании*">
+				<input type="text" placeholder="ФИО контактного лица*">
+				<input type="tel" placeholder="Телефон*">
+				<input type="email" placeholder="E-mail*" class="mail-arenda">
+				<input type="text" placeholder="Направление деятельности компании">
+				<input type="text" placeholder="Количество пользователей">
+			</div>
+			<div class="individual-solutions-right">
+				<select>
+                    <option>Выберите необходимый продукт</option>
+                    <option>Название продукта1</option>
+                    <option>Название продукта2</option>
+                    <option>Название продукта3</option>
+                </select>
+                <textarea placeholder="Дополнительные сведения"></textarea>
+			</div>
+			<div class="clear"></div>
+			<input type="submit" class="button-red button-arenda" value="Отправить заявку">
+			<div class="message-error-arenda">
+			    <span>Отмеченные поля заполнены неверно.</span>
+			</div>
+		</form>
+	</div>
+</div>
+<?php include("_footer.html"); ?>
