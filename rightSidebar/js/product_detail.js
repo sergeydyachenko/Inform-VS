b321bdb0f92a33826a8129f91aba39b868ba296d
SeryogaGit <seryoga_ne_@mailru>
2014-08-19 16:07:08 +0300

    edit product_detail

--- rightSidebar/js/product_detail.js
+++ rightSidebar/js/product_detail.js
@@ -68,4 +68,15 @@ $(document).ready(function(){
 			$(".aside-popup-send").show();
 		}
 	});
+	$(".questions-and-answers > div a").bind("click",questions);
+	function questions(e){
+		e.preventDefault()
+		$(this).toggleClass("questions-click");
+		if($(this).hasClass("questions-click")){
+			$(this).parent().children("p").slideDown()
+		}
+		else{
+			$(this).parent().children("p").slideUp()
+		}
+	}
 });
